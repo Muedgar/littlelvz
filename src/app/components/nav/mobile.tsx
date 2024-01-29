c1bb90edfd51485d4643f5b783bd9b2fbab56434
emutangana <emutangana@qtsoftwareltd.com>
2024-01-29 22:17:12 +0200

    User interface

--- src/app/components/nav/mobile.tsx
+++ src/app/components/nav/mobile.tsx
@@ -3,16 +3,17 @@ import { faFaceTired } from "@fortawesome/free-solid-svg-icons/faFaceTired";
 import { faPeopleGroup } from "@fortawesome/free-solid-svg-icons/faPeopleGroup";
 import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 export default function LittleMobile() {
   return (
     <div className="w-full h-fit flex flex-col justify-evenly items-center mt-[100px]">
-        <div className="flex w-fit mt-5 p-2 rounded-md bg-[#444]">
-            <FontAwesomeIcon
-                className="w-[25px] h-[25px] rounded-[50%]" 
-                icon={faHome} color="#37cbdf" />
-            <p className="text-white text-lg ml-2">Home</p>
-        </div>
+        <Link href="/asdf" className="flex w-fit mt-5 p-2 rounded-md bg-[#444]">
+    <FontAwesomeIcon
+      className="w-[25px] h-[25px] rounded-[50%]" 
+      icon={faHome} color="#37cbdf" />
+    <p className="text-white text-lg ml-2">Home</p>
+</Link>
 
         <div className="flex w-fit mt-5 p-2 rounded-md bg-[#444]">
             <FontAwesomeIcon
